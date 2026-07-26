ae68962070883dd0a4306f1d528d380822d95960
emiliefarah <emiliefarah@gmail.com>
2026-07-26 13:18:57 +0000

    Ajout du graphique du chiffre d'affaires par produit

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import plotly.express as px
 import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
+print(données)
+données['chiffre_affaires'] = données ['prix']*données ['qte']
+print(données)
+figure = px.pie(données, values='chiffre_affaires', names='produit', title="chiffre d'affaires par produit")
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+figure.write_html('chiffre-affaires-par-produit.html')
 
-figure.write_html('ventes-par-region.html')
-
-print('ventes-par-région.html généré avec succès !')
+print ('chiffre-affaires-par-produit.html')
